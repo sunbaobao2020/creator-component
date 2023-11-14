a6c3d3cc129d54de1bb93275d07f919f4dd66076
cannon <p12576999@gmail.com>
2023-11-14 17:35:24 +0800

    a

--- DetailTabs.vue
+++ DetailTabs.vue
@@ -22,21 +22,26 @@ const props = defineProps({
 <template>
     <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
     <ul class="flex flex-wrap -mb-px">
-        <li class="mr-2">
-            <Link :href="table_id ? route(`backend.${ table }.edit`, table_id) : route(`backend.${ table }.create`)" :class="[
-                action === 'basic_data' ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
-            ]" class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200">{{ $page.props.langs.basic_data }}</Link>
-        </li>
-        <li class="mr-2" v-for="(item, key) in add_tabs" :key="key" v-if="table_id">
-            <Link :href="item.route" :class="[
-                action === item.action ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
-            ]" class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200">{{ item.name }}</Link>
-        </li>
-        <li class="mr-2" v-if="table_id">
-            <Link :href="route('backend.audits.index', { table: table, table_id: table_id })" :class="[
-                action === 'audits' ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
-            ]"  class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200" aria-current="page">{{ $page.props.langs.audit }}</Link>
-        </li>
+        <template v-if="add_tabs.length > 0">
+            <li class="mr-2" v-for="(item, key) in add_tabs" :key="key">
+                <Link :href="item.route" :class="[
+                    action === item.action ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
+                ]" class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200">{{ item.name }}</Link>
+            </li>
+        </template>
+
+        <template v-else>
+            <li class="mr-2">
+                <Link :href="table_id ? route(`backend.${ table }.edit`, table_id) : route(`backend.${ table }.create`)" :class="[
+                    action === 'basic_data' ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
+                ]" class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200">{{ $page.props.langs.basic_data }}</Link>
+            </li>
+            <li class="mr-2" v-if="table_id">
+                <Link :href="route('backend.audits.index', { table: table, table_id: table_id })" :class="[
+                    action === 'audits' ? 'text-blue-600 dark:text-gray-200 border-blue-600 dark:border-gray-200': 'dark:text-gray-500 dark:border-gray-500'
+                ]"  class="inline-block p-4 border-b-2 rounded-t-lg active hover:text-blue-600 hover:border-blue-600 dark:hover:text-gray-200 dark:hover:border-gray-200" aria-current="page">{{ $page.props.langs.audit }}</Link>
+            </li>
+        </template>
     </ul>
     </div>
 </template>
