99a798d7684a6216a0e5521ae0f880981aa433c1
cannon <p12576999@gmail.com>
2024-02-02 15:00:26 +0800

    a

--- Edit.vue
+++ Edit.vue
@@ -4,14 +4,16 @@ import { PencilSquareIcon } from "@heroicons/vue/24/solid"
 const props = defineProps({
   item: { type: Object },
   permissions: { type: Boolean, default: true },
+  LinkHref: { type: String, default: null },
+  permissionsData: { type: String, default: null },
 })
 
 </script>
 
 <template>
     <Link
-      :href="route(`backend.${ $page.props.routeNameData }.edit`, item.id)"
-      v-if="!permissions || (permissions && ($page.props.permissions.includes(`edit ${ $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
+      :href="LinkHref || route(`backend.${ $page.props.routeNameData }.edit`, item.id)"
+      v-if="!permissions || (permissions && ($page.props.permissions.includes(`edit ${ permissionsData || $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
       class="inline-block pr-4 text-green-400 duration-100 rounded hover:text-green-600"
     >
       <PencilSquareIcon class="w-6 h-6" />
--- Read.vue
+++ Read.vue
@@ -5,14 +5,16 @@ import { EyeIcon } from "@heroicons/vue/24/solid"
 const props = defineProps({
   item: { type: Object },
   permissions: { type: Boolean, default: true },
+  LinkHref: { type: String, default: null },
+  permissionsData: { type: String, default: null },
 })
 
 </script>
 
 <template>
   <Link
-    :href="route(`backend.${ $page.props.routeNameData }.show`, item.id)"
-    v-if="!permissions || (permissions && ($page.props.permissions.includes(`read ${ $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
+    :href="LinkHref || route(`backend.${ $page.props.routeNameData }.show`, item.id)"
+    v-if="!permissions || (permissions && ($page.props.permissions.includes(`read ${ permissionsData || $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
     class="inline-block pr-4 duration-100 rounded hover:text-green-600"
   >
     <EyeIcon class="w-6 h-6" />
