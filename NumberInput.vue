a588790c09ee7f7ed14709223e63254124fc64f9
cannon <p12576999@gmail.com>
2024-04-11 11:30:00 +0800

    a

--- NumberInput.vue
+++ NumberInput.vue
@@ -1,26 +1,34 @@
 <script setup>
 import { ref, watch } from 'vue';
 import { ElInput } from 'element-plus';
+import { useCurrencyInput } from 'vue-currency-input'
 
 const props = defineProps({
   modelValue: { type: [String, Number] },
   placeholder: { type: String },
   disabled: { type: Boolean, default: false },
+  precision: { type: Number, default: 2 },
+  separator: { type: Boolean, default: true },
 })
+
+
+const options = {
+  currency: "TWD",
+  currencyDisplay: "hidden",
+  autoDecimalDigits: false,
+  useGrouping: props.separator,
+  accountingSign: false,
+  precision: props.precision,
+}
+
+const { inputRef } = useCurrencyInput(options, false)
+
 const emit = defineEmits(['update:modelValue'])
 
 const data = ref(props.modelValue);
 
 const emitUpdate = (value) => {
   emit('update:modelValue', value);
-};
-
-const formatNumber = (value) => {
-  return value ? parseFloat(value).toLocaleString('en-US') : '0';
-};
-
-const parseNumber = (value) => {
-  return (parseFloat(value.replace(/,/g, '')) || 0).toString();
 };
 
 watch([() => props.modelValue], () => {
@@ -34,8 +42,7 @@ watch([() => props.modelValue], () => {
     :placeholder="placeholder"
     class="w-full"
     :disabled="disabled"
-    :formatter="formatNumber"
-    :parser="parseNumber"
     @input="emitUpdate"
+    ref="inputRef"
   />
 </template>
